3c4873367945470d8ffe680f5ed89bf7ee02701e
Daniel Kreitsch <hello@danielkreitsch.com>
2023-12-04 05:41:19 +0100

    feat(build-executor): clone project to prevent single-instance collisions
    Each Unity project can only be opened once. To facilitate uninterrupted development while building, this update introduces a mechanism to clone the project for the build process. This ensures developers can continue working in Unity while a separate instance handles the build, avoiding instance collision and improving workflow efficiency.

--- src/executors/build/executor.ts
+++ src/executors/build/executor.ts
@@ -3,6 +3,10 @@ import { ExecutorContext } from "nx/src/config/misc-interfaces"
 import { executeCommand } from "../../utils/exec"
 import { FsTree } from "nx/src/generators/tree"
 import { getUnityBinaryPath } from "../../utils/platform"
+import * as path from "path"
+import { workspaceRoot } from "nx/src/utils/workspace-root"
+import { copySync } from "fs-extra"
+import { output } from "@nx/devkit"
 
 export default async function runExecutor(options: BuildExecutorSchema, context: ExecutorContext) {
   // Get the Unity version from the ProjectVersion.txt
@@ -11,10 +15,18 @@ export default async function runExecutor(options: BuildExecutorSchema, context:
   const versionFileContent = tree.read("ProjectSettings/ProjectVersion.txt").toString()
   const version = versionFileContent.split("\n")[0].split(" ")[1]
 
+  // Copy the project to a temporary directory
+  const tempProjectPath = path.join(workspaceRoot, "tmp", context.projectName)
+  const filter = (file) => {
+    return !file.startsWith(path.join(projectPath, "Temp/"))
+  }
+  output.log({ title: "Copying project to temporary directory", bodyLines: [tempProjectPath] })
+  copySync(projectPath, tempProjectPath, { filter })
+
   // Build the project using the Unity CLI
   const unityBinaryPath = getUnityBinaryPath(version)
-  const command = `"${unityBinaryPath}" -quit -batchmode -nographics -logFile - -executeMethod ${options.executeMethod} -projectPath "${projectPath}"`
-  const success = await executeCommand(command)
+  const command = `"${unityBinaryPath}" -quit -batchmode -nographics -logFile - -executeMethod ${options.executeMethod} -projectPath "${tempProjectPath}"`
+  let success = await executeCommand(command)
 
   return { success }
 }
