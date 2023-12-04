b943c9087f4e3bb10d72ee00707591f612140371
Daniel Kreitsch <hello@danielkreitsch.com>
2023-12-04 21:22:44 +0100

    fix(dependency-resolver): handle relative paths correctly
    refactor(unity-package):

---
+++ unity-package/Editor/Utility/NxUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace NxUnity
+{
+  public static class NxUtils
+  {
+    public static string GetWorkspaceRoot()
+    {
+      var currentDirectory = Directory.GetCurrentDirectory();
+      while (!File.Exists(Path.Combine(currentDirectory, "package.json")))
+      {
+        var parent = Directory.GetParent(currentDirectory);
+        if (parent == null)
+        {
+          throw new FileNotFoundException("Could not determine workspace root.");
+        }
+        currentDirectory = parent.FullName;
+      }
+      return currentDirectory;
+    }
+
+    public static string GetProjectRoot()
+    {
+      return Path.GetRelativePath(GetWorkspaceRoot(), Directory.GetParent(Application.dataPath)!.FullName).Replace('\\', '/');
+    }
+  }
+}
