493d88b9c0d6f20b794470038db551cc431d15bf
Daniel Kreitsch <hello@danielkreitsch.com>
2023-12-04 23:10:37 +0100

    feat: configure implicit dependencies on project or package generation
    This commit introduces a new feature that automatically configures implicit dependencies in an Nx workspace whenever a new Unity project or package is generated. Implicit dependencies are critical in Nx workspaces, as they help in understanding and managing the dependencies between projects and libraries within the workspace.

--- src/utils/workspace.ts
+++ src/utils/workspace.ts
@@ -1,4 +1,5 @@
-import { getProjects, readProjectConfiguration, Tree } from "@nx/devkit"
+import { getProjects, readProjectConfiguration, Tree, updateProjectConfiguration } from "@nx/devkit"
+import * as path from "path"
 
 function getUnityProjects(tree: Tree) {
   return getApplications(tree).filter((application) => {
@@ -17,6 +18,18 @@ function getApplications(tree: Tree) {
   return applications
 }
 
+function getUnityPackages(tree: Tree) {
+  return getLibraries(tree).filter((library) => {
+    const projectConfig = readProjectConfiguration(tree, library)
+    const packageJsonExists = tree.exists(path.join(projectConfig.sourceRoot, "package.json"))
+    const packageJsonMetaExists = tree.exists(
+      path.join(projectConfig.sourceRoot, "package.json.meta")
+    )
+    const runtimeFolderExists = tree.exists(path.join(projectConfig.sourceRoot, "Runtime"))
+    return packageJsonExists && (packageJsonMetaExists || runtimeFolderExists)
+  })
+}
+
 function getLibraries(tree: Tree) {
   const libraries = []
   getProjects(tree).forEach((project) => {
@@ -27,4 +40,20 @@ function getLibraries(tree: Tree) {
   return libraries
 }
 
-export { getUnityProjects, getApplications, getLibraries }
+/**
+ * Adds an implicit dependency to a project.
+ * @returns true if the dependency was added, false if it was already present
+ */
+function addImplicitDependency(tree: Tree, projectName: string, dependency: string): boolean {
+  const projectConfig = readProjectConfiguration(tree, projectName)
+  const implicitDependencies = projectConfig.implicitDependencies || []
+  if (!implicitDependencies.includes(dependency)) {
+    implicitDependencies.push(dependency)
+    projectConfig.implicitDependencies = implicitDependencies
+    updateProjectConfiguration(tree, projectName, projectConfig)
+    return true
+  }
+  return false
+}
+
+export { getUnityProjects, getApplications, getUnityPackages, getLibraries, addImplicitDependency }
